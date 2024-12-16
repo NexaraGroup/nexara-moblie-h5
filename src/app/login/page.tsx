a6e4f9bfeaff66c11b381ee69126d67197d659ed
yy <yanyang1116@gmail.com>
2024-12-16 12:06:33 +0800

    feat: 提交

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,7 +24,9 @@ const PageLogin = () => {
 	};
 
 	return (
-		<div className={`flex flex-col justify-between h-[calc(100vh-48px)]`}>
+		<div
+			className={`flex flex-col justify-between h-[calc(100vh-48px-env(safe-area-inset-top)-env(safe-area-inset-bottom))]`}
+		>
 			<div>
 				<h2
 					className="text-[28px] font-bold
