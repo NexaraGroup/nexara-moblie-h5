45dafa79bd923bda2aac7b9fae9a0a36a6ddfc0f
yy <yanyang1116@gmail.com>
2024-12-18 15:26:25 +0800

    feat: 下个礼拜继续

--- src/components/render-wrapper/index.tsx
+++ src/components/render-wrapper/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { NO_AUTH_ROUTES } from '@/config/base';
 import type { Locale } from '@/i18n/config';
 import { px2remTransformer, StyleProvider } from '@ant-design/cssinjs';
 import { ConfigProvider, SafeArea } from 'antd-mobile';
@@ -8,30 +9,62 @@ import enUS from 'antd-mobile/es/locales/en-US';
 import zhCN from 'antd-mobile/es/locales/zh-CN';
 import zhHK from 'antd-mobile/es/locales/zh-HK';
 import { ThemeProvider } from 'next-themes';
+import { usePathname, useRouter } from 'next/navigation';
 import { useImmer } from 'use-immer';
 
 const px2rem = px2remTransformer({
 	rootValue: Number(process.env.PX_TO_REM_ROOT_VALUE_WITH_DESIGN_WIDTH),
 });
 
+const localMap = new Map<Locale, typeof enUS | typeof zhCN | typeof zhHK>([
+	['en-US', enUS],
+	['zh-CN', zhCN],
+	['zh-HK', zhHK],
+]);
+
 export default function RenderWrapper(props: React.PropsWithChildren<{ locale: Locale }>) {
-	const [loaded, setLoaded] = useImmer(false);
-	const localMap = new Map<Locale, typeof enUS | typeof zhCN | typeof zhHK>([
-		['en-US', enUS],
-		['zh-CN', zhCN],
-		['zh-HK', zhHK],
-	]);
+	const [uiDepLoaded, setUiDepLoaded] = useImmer(false);
+	const [authCheckLoaded, setAuthCheckLoaded] = useImmer(false);
+	const pathname = usePathname();
+	const router = useRouter();
+	const didRenderCheck = useRef(false);
 
 	useEffect(() => {
+		if (didRenderCheck.current) return;
+		didRenderCheck.current = true;
+
+		if (NO_AUTH_ROUTES.includes(pathname)) {
+			setAuthCheckLoaded(true);
+		} else {
+			// api 检查 token
+			// 如果鉴定不成功，则统一取登录页面
+			if (false) {
+				const fullPath = window.location.href.split(window.location.origin)[1];
+				const redirect = encodeURIComponent(fullPath);
+				router.replace(`/sign-up?redirect=${redirect}`);
+				setAuthCheckLoaded(true);
+				return;
+			}
+			// 如果成功了，但是检测出来，没绑定 ga，则去绑定 ga 页面
+			if (true) {
+				const fullPath = window.location.href.split(window.location.origin)[1];
+				const redirect = encodeURIComponent(fullPath);
+				router.replace(`/bind-ga?redirect=${redirect}`);
+				setAuthCheckLoaded(true);
+				return;
+			}
+			setAuthCheckLoaded(true);
+		}
+
 		// @ts-ignore
 		import('@/utils/amfe-flexible-custom.js').finally(() => {
-			setLoaded(true);
+			setUiDepLoaded(true); // 即使路由跳转，这个也不会受到影响
 		});
-	}, []);
+	}, [pathname]);
 
 	return (
 		<>
-			{loaded ? (
+			{uiDepLoaded && authCheckLoaded ? (
 				<StyleProvider transformers={[px2rem]}>
 					<ThemeProvider defaultTheme="light">
 						<ConfigProvider locale={localMap.get(props.locale)}>
--- src/config/base.tsx
+++ src/config/base.tsx
@@ -2,3 +2,12 @@ export const I18N_COOKIE_NAME = 'NEXARA_H5_LOCALE_V1';
 export const LOCAL_STORE_NAME = 'NEXARA_H5_LOCAL_STORE_NAME_V1';
 
 export const COMMON_FIELD_MAX_LENGTH = 50;
+
+export const NO_AUTH_ROUTES = [
+	'/sign-up',
+	'/set-password',
+	'/login',
+	'/email-verify',
+	'/feedback',
+	'/forgot-password',
+];
